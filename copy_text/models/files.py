751fb86c8dc95c7acdb29a9e75e72f7518788458
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-27 14:26:37 +0200

    Update files.py

--- copy_text/models/files.py
+++ copy_text/models/files.py
@@ -5,3 +5,10 @@ from odoo import fields, models
 _logger = logging.getLogger(__name__)
 
 
+
+
+
+## https://www.odoo.com/forum/help-1/write-to-a-local-file-28653
+
+
+## http://pymotw.com/2/tempfile/
